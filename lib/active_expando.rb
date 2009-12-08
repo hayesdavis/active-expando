b6b8895e209bb08b5162fad44f5d210b152f8cb9
Hayes Davis <hayes@appozite.com>
2009-12-07 23:33:00 -0600

    Fixed requires in main active_expando file

--- lib/active_expando.rb
+++ lib/active_expando.rb
@@ -1,3 +1,4 @@
+require 'active_expando/expando_store/aliasing'
 require 'active_expando/expando_store'
 require 'active_expando/active_record/config'
 require 'active_expando/active_record/active_record'
