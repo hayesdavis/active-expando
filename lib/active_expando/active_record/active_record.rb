ffc6a1b5aa615abc566b5f2df2d792b829ad1db2
Hayes Davis <hayes@appozite.com>
2009-12-11 16:38:35 -0600

    Added hook to destroy the expandos object if the active record object is destroyed and the ability to specify options to be passed to ActiveRecord finders when using expando_* methods. Cleaned up the conditions under which validations will be performed on the expandos. They'll always be performed on a new record.

--- lib/active_expando/active_record/active_record.rb
+++ lib/active_expando/active_record/active_record.rb
@@ -7,6 +7,7 @@ module ActiveExpando
         include InstanceMethods
         before_save :validate_expandos
         after_save :save_expandos
+        after_destroy :destroy_expandos
       end
     end
   
@@ -47,8 +48,9 @@ module ActiveExpando
       
       def method_missing(name,*args)
         if name.to_s =~ /expando_(.+)/
+          ar_options = extract_ar_options!(args)
           result = expandos_class.send($1.to_sym,*args)
-          result ? convert_result(result) : nil
+          result ? convert_result(result,ar_options) : nil
         else
           super(name,*args)
         end
@@ -59,16 +61,14 @@ module ActiveExpando
       end
       
       private
-        def convert_result(result)
+        def convert_result(result,ar_options={})
           if result.kind_of?(expandos_class)
-            ar_res = find(:first,:conditions=>{self.primary_key=>result.id})
-            ar_res.send(:expandos=,result)
-            ar_res
+            expando_to_active_record(result,ar_options)
           elsif paginated?(result)
-            result.subject = expandos_to_active_records(result.subject)
+            result.subject = expandos_to_active_records(result.subject,ar_options)
             result
           elsif enumerable_of_expando_stores?(result)
-            expandos_to_active_records(result)
+            expandos_to_active_records(result,ar_options)
           else
             result
           end
@@ -79,13 +79,46 @@ module ActiveExpando
           #Use any? because it's reasonable to assume that if 1 is, they all are
           result.any?{|item| item.kind_of?(expandos_class)}
         end
+        
+        def expando_to_active_record(result,ar_options={})
+          ar_options = merge_active_record_options({
+            :conditions=>{self.primary_key=>result.id}
+          },ar_options)
+          ar_res = find(:first,ar_options)
+          if ar_res
+            ar_res.send(:expandos=,result)
+          end
+          ar_res          
+        end
       
-        def expandos_to_active_records(exps)
+        def expandos_to_active_records(exps,ar_options={})
           by_id_map = exps.inject({}) {|h,k| h[k.id] = k; h }
           ids = exps.map {|r| r.id }
-          find(:all,:conditions=>{self.primary_key=>ids}).each do |t|
+          ar_options = merge_active_record_options({
+            :conditions=>{self.primary_key=>ids}
+          },ar_options)
+          find(:all,ar_options).each do |t|
             t.send(:expandos=,by_id_map[t.id])
           end
+        end
+        
+        def merge_active_record_options(ar1,ar2)
+          merged = ar1.dup
+          ar2.each do |k,v|
+            v1 = merged[k]
+            if v1.kind_of?(Hash) && v.kind_of?(Hash)
+              merged[k] = merge_active_record_options(v1,v)
+            else
+              merged[k] = v
+            end
+          end
+          merged
+        end
+        
+        def extract_ar_options!(args)
+          ar_options = args.last.kind_of?(Hash) ? 
+            args.last.delete(:ar_options) : nil
+          ar_options || {}
         end
       
         # Nasty hack to check for whether the result is a 
@@ -148,14 +181,26 @@ module ActiveExpando
         end
         
         def validate_expandos
-          return true unless expandos_loaded?
-          unless @expandos.valid?
-            @expandos.errors_with_aliases.each do |attr, msgs|
+          return true unless should_validate_expandos?
+          unless expandos.valid?
+            expandos.errors_with_aliases.each do |attr, msgs|
               Array(msgs).each do |msg|
                 self.errors.add(attr,msg)
               end
             end
             return false
+          end
+        end
+        
+        def should_validate_expandos?
+          if self.class.expandos_class.validations.empty?
+            # Don't bother validating if there's no validations to be done
+            false
+          else
+            # Always validate a new record but only validate expandos if 
+            # for an existing record if they've been loaded up and potentially 
+            # changed
+            new_record? || expandos_loaded?
           end
         end
         
@@ -165,6 +210,10 @@ module ActiveExpando
             @expandos.id = self[self.class.primary_key]
           end
           @expandos.save
+        end
+
+        def destroy_expandos
+          expandos.destroy unless expandos.new_record?
         end
         
         def expandos=(expandos_store)
